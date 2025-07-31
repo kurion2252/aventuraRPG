d71fd7dc36794df9107dc3de04171b8606263de8
kurion2252 <120735031+kurion2252@users.noreply.github.com>
2025-07-31 20:28:03 -0300

    31/07/2025

--- js/aventura.js
+++ js/aventura.js
@@ -1,71 +1,82 @@
 // simulador RPG interactivo //
 
-const dañoBAse = 20;
-let player = prompt("¿Cual es tu nombre, Aventurer@?");
-let healtp = 100;
-let nivelp = 1;
-let healten = 100;
-let damageen = 10
+// Autor: [Brian]
+// funciones constructoras para jugador
+function Jugador(nombre, daño,defensa, vida, nivel) {
+    this.nombre = nombre;
+    this.daño = daño;
+    this.defensa = defensa;
+    this.vida = vida;
+    this.nivel = nivel;
 
+    this.atacar = function(enemigos) { 
+        let dañoreal = this.daño - enemigos.defensa;
+        if (dañoreal < 0) dañoreal = 0; // daño mínimo
+        enemigos.vida -= dañoreal; // resta el daño al enemigo
+        return `${this.nombre} ataca a ${enemigos.nombre} y le causa ${dañoreal} puntos de daño.`;
+        
+        this.curarse = function() {
+            if(this.defensa >= 5) {
+                this.vida += 20; // curación si la defensa es alta
+                if  (this.vida > 100) this.vida = 100; // máximo de vida    
+                this.defensa -= 5; // reduce defensa al curarse
+                return `${this.nombre} se cura 20 puntos de vida.`;}
+            else {
+                return `${this.nombre} no puede curarse porque su defensa es baja.`;
+            }    
+        }     
+    }
+};
 
-//enemigos //
+// funciones constructoras para enemigos
+function Enemigo(nombre, daño, defensa, salud, nivel) {
+    this.nombre = nombre;
+    this.daño = daño;
+    this.defensa = defensa;
+    this.vida = salud;
+    this.nivel = nivel;
+}
+
+// crear juagor y enemigos
+let jugador = new Jugador("Heroe", 25, 15, 100, 1);
 
 let enemigos = [
-    {nombre: "Goblin", healten: 30, damageen: 5 }, 
-    {nombre: "Hobgoblin", healten: 60, damageen: 10},
-    {nombre: "Lobo", healten: 40, damageen: 15}];
+    new Enemigo("goblin con espada", 5, 5, 15, 1),
+    new Enemigo("goblin arquero", 7, 5, 20, 3),
+    new Enemigo("Hechicero goblin", 10, 5, 20, 5),
+    new Enemigo("hobgoblin", 20, 15, 30, 11),
+    new Enemigo("Sabio goblin", 40, 20, 30, 15),
+    new Enemigo("Rey goblin", 60, 55, 50, 20),
+    new Enemigo("Lobo", 4, 2, 10 , 2),
+];
 
-function atacar(enemigo) {
-    alert (`${player} ataca al ${enemigo.nombre}!`);
-    enemigo.healten -= dañoBAse;
-    if (enemigo.healten <=0) {
-        alert(`${enemigo.nombre} fue derrotado!`);
-        return true;
-    } else {
-        alert(`${enemigo.nombre} aun le quedan ${enemigo.healten} de salud.`);
-        return false;
+//bucle principal peleas
+for (let i = 0; i < enemigos.length; i++) {
+    let enemigo = enemigos[i];
+    alert(`Un ${enemigo.nombre} salvaje aparece!`);
+    while (enemigo.vida > 0 && jugador.vida > 0) {
+        let accion = prompt(`¿Qué deseas hacer con ${enemigo.nombre}?\n` +`1- Atacar\n2 - Curarse\n3 - ver Estado\n4 - Huir`);
+
+        if (accion === '1') {
+            alert(jugador.atacar(enemigo));
+        }  else if (accion === '2') {
+            alert(jugador.curarse());
+        } else if (accion === '3') {
+            alert (
+           `${jugador.nombre} - Nv. ${jugador.nivel}\n` + `salud: ${jugador.vida} | Daño: ${jugador.daño} | Defensa: ${jugador.defensa}\n\n` + `${enemigo.nombre} - NV. ${enemigo.nivell}\n` + `Salud: ${enemigo.salud} |Daño: ${enemigo.daño} | Defensa: ${enemigo.defensa}`
+        );
+        continue;
+    } else if(accion === "4"){ 
+        alert("Has huido del combate! los enemigos celebran la victoria...");
     }
+//estadisticas del jugador y enemigos
+const estadisticasJugador = () => {
+    console.log(`Jugador: ${jugador.nombre}`);
+    console.log(`Nivel: ${jugador.nivel} | Vida: ${jugador.vida} | Daño: ${jugador.daño} | Defensa: ${jugador.defensa}`);
+};
+const estadisticasEnemigos = () => {
+    enemigos.forEach(enemigo => {
+        console.log(`Enemigo: ${enemigo.nombre}`);
+        console.log(`Nivel: ${enemigo.nivel} | Vida: ${enemigo.vida} | Daño: ${enemigo.daño} | Defensa: ${enemigo.defensa}`);
+    });
 }
-
-function recibirDaño(enemigo) {
-    alert (`${enemigo.nombre} contraataca y causa ${enemigo.damageen} de daño.`); healtp-= enemigo.damageen;
-    if (healtp <=0){
-        alert(`${player} ha sido derrotado...`);
-    } else {
-        alert( `A ${player} le quedan ${healtp} de salud.`);
-    }
-}
-
-function iniciarAventura() {
-    alert(`¡Bienvenid@, ${player}! Que comience tu aventura!!.`);
-
-    let abandono = false;
-    
-    for (let i = 0; i <enemigos.length; i++) {
-       let enemigo = enemigos[i];
-
-       let pelear = confirm(`Encontraste un ${enemigo.nombre}, ¿quieres pelear con el?.`);
-
-       if (!pelear) {
-        alert(`${player} ha huido del combate, Los enemigos ganan la partida`);
-        abandono = true;
-        break;
-       }
-        
-       while (healtp > 0 && enemigo.healten >0) {
-        const vencido = atacar(enemigo);
-        if (vencido) {
-            nivelp++;
-            alert(`Felicitaciones has subido de nivel. Nivel ${nivelp}!`);
-            break;
-        }
-        recibirDaño(enemigo);
-       }
-       if (healtp <=0) break;
-    }
-    if (healtp>0 && !abandono) {
-        alert(`¡${player} ha vencido a todos los enemigos!`);
-    }
-}
-
-iniciarAventura();
